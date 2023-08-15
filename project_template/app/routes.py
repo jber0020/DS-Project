4bf29f3e318361c9ed69b7f6b01a0d06721b1134
Ryan Hendler <72334202+Razzahen@users.noreply.github.com>
2023-08-15 12:48:29 +1000

    upload template for group with cicd

---
+++ project_template/app/routes.py
@@ -0,0 +1,14 @@
+
+from flask import render_template, jsonify
+from app import app
+import sys
+sys.path.append('../scripts')
+# import your scripts here
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/sample_endpoint')
+def sample_endpoint():
+    return jsonify({"message": "Hello from the API!"})
