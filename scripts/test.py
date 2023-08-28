91b7ab1e4f7cc5c3d0749c5434bee90a64e97d9d
Ryan Hendler <72334202+Razzahen@users.noreply.github.com>
2023-08-28 22:59:35 +1000

    lstm code

--- scripts/test.py
+++ scripts/test.py
@@ -1,113 +1,259 @@
-import tensorflow as tf
-from tensorflow import keras
-import pandas as pd
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
 from utils import *
-from kerastuner.tuners import RandomSearch
+from tensorflow.keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.model_selection import GridSearchCV
 
-data = pd.read_csv(get_root('data/elec_p4_dataset/Train/merged_actuals.csv'))
+import tensorflow as tf
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.regularizers import l2
 
-data['Time'] = pd.to_datetime(data['Time'])
-
-# Clean time values
-data['Day'] = data['Time'].dt.day
-data['Month'] = data['Time'].dt.month
-data['Hour'] = data['Time'].dt.hour
-data['Year'] = data['Time'].dt.year
-data['Weekday'] = data['Time'].dt.weekday
-data.drop("Time", axis="columns", inplace=True)
-
-# Normalisation
-normalised_columns = ~data.columns.isin(['Hour', 'Month', 'Load (kW)'])
-data.loc[:,normalised_columns] = (data.loc[:,normalised_columns]-data.loc[:,normalised_columns].mean()) / data.loc[:,normalised_columns].std()
-
-# Sine/Cosine Encoding
-data['Hour Sin'] = np.sin(data['Hour'] * 2 * np.pi / 24)
-data['Hour Cos'] = np.cos(data['Hour'] * 2 * np.pi / 24)
-data['Month Sin'] = np.sin(data['Month'] * 2 * np.pi / 12)
-data['Month Cos'] = np.cos(data['Month'] * 2 * np.pi / 12)
-data.drop(['Hour', 'Month'], axis='columns', inplace=True)
-
-# Remove unimportant columns
-data.drop(['Pressure_kpa', 'Cloud Cover (%)', 'Humidity (%)'], axis='columns', inplace=True)
-
-def Sequential_Input(df, input_sequence, output_sequence):
-    df_np = df.to_numpy()
-    X = []
-    y = []
-    
-    for i in range(len(df_np) - input_sequence - output_sequence + 1):
-        row = [a for a in df_np[i:i + input_sequence]]
-        X.append(row)
-        label = df_np[i + input_sequence:i + input_sequence + output_sequence, df.columns.get_loc("Load (kW)")]  # Extract Load (kW) values
-        y.append(label)
+class ImprovedLSTMForecaster:
+    def __init__(self, input_seq_len=72, output_seq_len=1, n_features=17):
+        self.input_seq_len = input_seq_len
+        self.output_seq_len = output_seq_len
+        self.n_features = n_features
+        self.scaler = MinMaxScaler()
+        self.model = None
         
-    return np.array(X), np.array(y)
+    def preprocess_data(self, df, test_size=0.2):
+        df['Time'] = pd.to_datetime(df['Time'])
 
-X, y = Sequential_Input(data, 24, 48)
+        # Adding lags
+        for i in [1, 2, 7]:
+            col_name = f"Lag_{i}day"
+            df[col_name] = df['Time'].apply(lambda x: df.loc[df['Time'] == (x - pd.DateOffset(days=i)), 'Load (kW)'].values[0] 
+                                if (x - pd.DateOffset(days=i)) in df['Time'].values else np.nan)
 
-n=len(X)
+        # Drop first week of data due to NaNs from lag
+        cutoff = df['Time'].dt.date.min() + pd.DateOffset(days=7)
+        df = df[df['Time'].dt.date >= cutoff.date()]
 
-# Training data
-X_train, y_train = X[:int(n*0.7)], y[:int(n*0.7),]
+        # Decomposing datetime
+        df['Day'] = df['Time'].dt.day
+        df['Month'] = df['Time'].dt.month
+        df['Hour'] = df['Time'].dt.hour
+        df['Year'] = df['Time'].dt.year
+        df['Weekday'] = df['Time'].dt.weekday
 
-# Validation data
-X_val, y_val = X[int(n*0.7):int(n*0.9)], y[int(n*0.7):int(n*0.9),]
+        # Sine/Cosine Encoding for hour and month
+        df['Hour Sin'] = np.sin(df['Hour'] * 2 * np.pi / 24)
+        df['Hour Cos'] = np.cos(df['Hour'] * 2 * np.pi / 24)
+        df['Month Sin'] = np.sin(df['Month'] * 2 * np.pi / 12)
+        df['Month Cos'] = np.cos(df['Month'] * 2 * np.pi / 12)
+        df.drop(['Hour', 'Month', 'Time'], axis='columns', inplace=True)
 
-# Test data
-X_test, y_test = X[int(n*0.9):], y[int(n*0.9):,]
+        # Scale the data
+        load_data = df['Load (kW)'].values
+        features = df.drop(['Load (kW)'], axis=1).values
+        scaled_features = self.scaler.fit_transform(features)
 
-n_features = X_train.shape[2]                       
+        # Concatenate Load (unscaled) with the scaled features
+        data = np.column_stack((load_data, scaled_features))
 
-"""
-model1 = tf.keras.Sequential()
+        # Sequence the data for LSTM
+        X, y = [], []
+        for i in range(len(data) - self.input_seq_len - self.output_seq_len + 1):
+            X.append(data[i:i+self.input_seq_len])
+            y.append(data[i+self.input_seq_len:i+self.input_seq_len+self.output_seq_len, 0])
 
-model1.add(tf.keras.layers.InputLayer((24,n_features)))
-model1.add(tf.keras.layers.LSTM(100, return_sequences = True))     
-model1.add(tf.keras.layers.LSTM(100, return_sequences = False))
-model1.add(tf.keras.layers.Dense(48, activation = 'linear'))
+        X = np.array(X)
+        y = np.array(y)
 
-early_stop = tf.keras.callbacks.EarlyStopping(monitor = 'val_loss', patience = 2)
+        X_train_full, X_test, y_train_full, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+        X_train, X_val, y_train, y_val = train_test_split(X_train_full, y_train_full, test_size=test_size, random_state=42)
 
-model1.compile(loss = 'mean_squared_error', 
-               optimizer = 'Adam', 
-               metrics = ['mse', 'mae', 'mape'])
+        return (X_train, y_train), (X_val, y_val), (X_test, y_test)
 
-model1.fit(X_train, y_train, 
-           validation_data = (X_val, y_val), 
-           epochs = 50, 
-           callbacks = [early_stop])
-"""
-def build_lstm_model(hp):
-    model = keras.Sequential()
     
-    # Define hyperparameters
-    units = hp.Int('units', min_value=32, max_value=256, step=32)
-    num_layers = hp.Int('num_layers', min_value=1, max_value=3)
+    def build_model(self, lstm_layers=(50,), dropout=0.2, learning_rate=0.001, activation='tanh', regularization_rate=0.01):
+        model = Sequential()
+        
+        for i, units in enumerate(lstm_layers):
+            return_seq = True if i < len(lstm_layers) - 1 else False
+            if i == 0:
+                model.add(LSTM(units, activation=activation, input_shape=(self.input_seq_len, self.n_features),
+                               return_sequences=return_seq, kernel_regularizer=l2(regularization_rate)))
+            else:
+                model.add(LSTM(units, activation=activation, return_sequences=return_seq, kernel_regularizer=l2(regularization_rate)))
+            model.add(Dropout(dropout))
+        
+        model.add(Dense(self.output_seq_len))
+        optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+        model.compile(optimizer=optimizer, loss='mse', metrics=['mse', 'mape'])
+        self.model = model
+        return model
     
-    for _ in range(num_layers-1):
-        model.add(keras.layers.LSTM(units, return_sequences=True))
-    model.add(keras.layers.LSTM(units, return_sequences=False))
-    model.add(keras.layers.Dense(48, activation='linear'))
+    def train(self, X_train, y_train, X_val, y_val, epochs=10, batch_size=64, patience=5):
+        if self.model is None:
+            print("Model not built yet. Call `build_model` first.")
+            return
+        
+        early_stop = EarlyStopping(monitor='val_loss', patience=patience, restore_best_weights=True)
+        
+        history = self.model.fit(
+            X_train, y_train,
+            epochs=epochs,
+            batch_size=batch_size,
+            validation_data=(X_val, y_val),
+            callbacks=[early_stop]
+        )
+        return history
     
-    model.compile(
-        loss='mean_squared_error',
-        optimizer=keras.optimizers.Adam(learning_rate=hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])),
-        metrics=['mse', 'mae', 'mape']
-    )
+    def predict(self, X):
+        if self.model is None:
+            print("Model not trained yet. Train the model first.")
+            return
+        y_pred = self.model.predict(X)
+        # Since the Load predictions aren't scaled, we directly return them
+        return y_pred[:, 0]
     
-    return model
+    def tune_hyperparameters(self, X, y, param_grid):
+        model = KerasRegressor(build_fn=self.build_model, epochs=10, batch_size=64, verbose=1)
+        
+        grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=2)
+        grid_result = grid.fit(X, y)
+        
+        # Return the best parameters from the grid search
+        return grid_result.best_params_
 
-tuner = RandomSearch(
-    build_lstm_model,
-    objective='val_loss',
-    max_trials=10,  # Number of trials to search
-    directory='lstm_tuning',  # Directory to store the search results
-    project_name='lstm_tuning'  # Name of the tuning project
-)
+df = pd.read_csv(get_root('data/elec_p4_dataset/Train/merged_actuals.csv'))
 
-tuner.search(X_train, y_train, validation_data=(X_val, y_val), epochs=50, callbacks=[tf.keras.callbacks.EarlyStopping('val_loss', patience=3)])
+# Test the improved class
+forecaster_improved = ImprovedLSTMForecaster(input_seq_len=24, output_seq_len=1, n_features=17)
 
-best_model = tuner.get_best_models(num_models=1)[0]
-evaluation = best_model.evaluate(X_test, y_test)
-print(evaluation)
+# Splitting the data
+(X_train, y_train), (X_val, y_val), (X_test, y_test) = forecaster_improved.preprocess_data(df, test_size=0.2)
+
+# Parameters for testing
+param_grid_improved = {
+    'lstm_layers': [(64, 128)],
+    'dropout': [0.2],
+    'learning_rate': [0.01],
+    'activation': ['tanh'],
+    'regularization_rate': [0.01]
+}
+
+# Tune hyperparameters
+best_params_improved = forecaster_improved.tune_hyperparameters(X_train, y_train, param_grid_improved)
+
+# Using the best_params, build the model
+forecaster_improved.build_model(**best_params_improved)
+
+# Train the model on the training set using the best hyperparameters
+forecaster_improved.train(X_train, y_train, X_val, y_val, epochs=10, batch_size=64, patience=5)
+
+# Predict using the model on the test set
+y_pred_improved = forecaster_improved.predict(X_test)
+
+class LSTMForecaster:
+    def __init__(self, input_seq_len=72, output_seq_len=1, n_features=7):
+        self.input_seq_len = input_seq_len
+        self.output_seq_len = output_seq_len
+        self.n_features = n_features
+        self.scaler = MinMaxScaler()
+        self.model = None
+        
+    def preprocess_data(self, df, test_size=0.2):
+        df['Time'] = pd.to_datetime(df['Time'])
+        df = df.sort_values(by='Time')
+        
+        # Extract Load and features
+        load_data = df['Load (kW)'].values
+        features = df.drop(['Time', 'Load (kW)'], axis=1).values
+        
+        # Scale only the features
+        scaled_features = self.scaler.fit_transform(features)
+        
+        # Concatenate Load (unscaled) with the scaled features
+        data = np.column_stack((load_data, scaled_features))
+        
+        X, y = [], []
+        for i in range(len(data) - self.input_seq_len - self.output_seq_len + 1):
+            X.append(data[i:i+self.input_seq_len])
+            y.append(data[i+self.input_seq_len:i+self.input_seq_len+self.output_seq_len, 0])
+        
+        X = np.array(X)
+        y = np.array(y)
+        
+        X_train_full, X_test, y_train_full, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+        X_train, X_val, y_train, y_val = train_test_split(X_train_full, y_train_full, test_size=test_size, random_state=42)
+        
+        return (X_train, y_train), (X_val, y_val), (X_test, y_test)
+    def build_model(self, lstm_units=50, dropout=0.2, learning_rate=0.001, activation='tanh'):
+        model = Sequential()
+        model.add(LSTM(lstm_units, activation=activation, input_shape=(self.input_seq_len, self.n_features), return_sequences=True))
+        model.add(Dropout(dropout))
+        model.add(LSTM(lstm_units, activation=activation))
+        model.add(Dropout(dropout))
+        model.add(Dense(self.output_seq_len))
+        model.compile(optimizer='adam', loss='mse', metrics=['mse', 'mape'])
+        self.model = model
+        return model
+    
+    def train(self, X_train, y_train, X_val, y_val, epochs=10, batch_size=64):
+        if self.model is None:
+            print("Model not built yet. Call `build_model` first.")
+            return
+        
+        history = self.model.fit(
+            X_train, y_train,
+            epochs=epochs,
+            batch_size=batch_size,
+            validation_data=(X_val, y_val)
+        )
+        return history
+    
+    def predict(self, X):
+        if self.model is None:
+            print("Model not trained yet. Train the model first.")
+            return
+        y_pred = self.model.predict(X)
+        # Since the Load predictions aren't scaled, we directly return them
+        return y_pred[:, 0]
+
+    
+    def tune_hyperparameters(self, X, y, param_grid):
+        model = KerasRegressor(build_fn=self.build_model, epochs=10, batch_size=64, verbose=1)
+        
+        grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=3)
+        grid_result = grid.fit(X, y)
+        
+        # Return the best parameters from the grid search
+        return grid_result.best_params_
+
+
+# print("hello")
+# # Make sure the dataset columns are in the correct order and sorted by time.
+# df = pd.read_csv(get_root('data/elec_p4_dataset/Train/merged_actuals.csv'))
+# print("read data")
+# # Initialize the LSTMForecaster
+# forecaster = LSTMForecaster(input_seq_len=72, output_seq_len=48, n_features=7)
+
+# # Preprocess the data and split into train, validation, and test sets
+# (X_train, y_train), (X_val, y_val), (X_test, y_test) = forecaster.preprocess_data(df, test_size=0.2)
+# print(X_train[:1], X_test[:1])
+
+# # Define a parameter grid for hyperparameter tuning
+# param_grid = {
+#     'lstm_units': [25],
+#     'dropout': [0.2],
+#     'learning_rate': [0.1],
+#     'activation': ['tanh']
+# }
+
+# # Tune hyperparameters
+# best_params = forecaster.tune_hyperparameters(X_train, y_train, param_grid)
+
+# # Using the best_params, build the model
+# forecaster.build_model(**best_params)
+
+# # Train the model on the training set using the best hyperparameters
+# forecaster.train(X_train, y_train, X_val, y_val, epochs=10, batch_size=64)
+
+# # Predict using the model on the test set
+# y_pred = forecaster.predict(X_test)
